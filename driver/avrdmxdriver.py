72e1eb754c13c512a8b4721b2338367f3e3735d7
Matt Gruskin <matthew.gruskin@gmail.com>
2012-10-04 21:21:01 -0400

    configurable framerate

--- driver/avrdmxdriver.py
+++ driver/avrdmxdriver.py
@@ -13,13 +13,17 @@ if __name__ == "__main__":
   libc = CDLL("libc.so.6")
   libc.unlockpt(master_fd)
 
+  universe_fps = {}
   def Callback(universe, channels):
+    universe_fps[universe] = universe_fps.get(universe, 0) + 1
     serial_dmx.SendChannels(channels, universe=universe)
   sacn_listener = sacn.SACNListener(universes=[1,2,3,4], callback=Callback)
 
   try:
     while True:
       time.sleep(1)
+      logging.info('FPS: %s', str(universe_fps))
+      universe_fps.clear()
   finally:
     sacn_listener.Close()
     serial_dmx.Close()
